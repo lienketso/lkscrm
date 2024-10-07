0e5913d5cdc5cb95ecd6cbc451d24f12a3f6e78c
lienketso <thanhan1507@gmail.com>
2024-10-07 16:14:24 +0700

    aaaa

--- packages/Webkul/User/src/Database/Migrations/2021_03_12_074857_create_users_table.php
+++ packages/Webkul/User/src/Database/Migrations/2021_03_12_074857_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->nullable();
             $table->string('password')->nullable();
             $table->boolean('status')->default(0);
             $table->integer('role_id')->unsigned();
