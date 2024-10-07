04c0f690779959dd6a11346b89556bd4ed31a4a5
lienketso <thanhan1507@gmail.com>
2024-10-07 16:03:53 +0700

    ssss

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->id();
-            $table->string('uuid',199)->unique();
+            $table->string('uuid', 255)->unique();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
