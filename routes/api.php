6fcbe67f4e5081e7565e0a3a8ce2589a0f33a7f2
annp-tiny <annp>
2024-12-25 03:45:08 +0700

    update webhook

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Webkul\Admin\Http\Controllers\Lead\ZaloTemplateController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/zns-webhook', [ZaloTemplateController::class, 'handleWebhook'])->name('zns.handleWebhook');
